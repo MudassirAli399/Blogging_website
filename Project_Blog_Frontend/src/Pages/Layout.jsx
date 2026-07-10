684c720b2ac56b245de69ae0afaee0b29dec06bb
Mudassir Ali <muddasira435@gmail.com>
2026-07-11 00:29:07 +0500

    ...

--- Project_Blog_Frontend/src/Pages/Layout.jsx
+++ Project_Blog_Frontend/src/Pages/Layout.jsx
@@ -3,7 +3,7 @@ import Header from "./Header"
 import {Outlet} from "react-router-dom"
 import { useSelector } from "react-redux";
 export default function Layout(){
-    const loader = useSelector((state)=>state.Authentication.loader)
+    
     return(
         <>
         
